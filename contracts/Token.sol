589ccd2f6562ce5ca89b40e05127a4f38c863542
jimjimvalkema <jjvalkema@protonmail.com>
2025-01-09 19:49:17 +0100

    laid out changes needed to support partial withdraws

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -19,7 +19,9 @@ interface IVerifier {
 error VerificationFailed();
 
 contract Token is ERC20, Ownable {
-    mapping(bytes32 => bool) public nullifiers;
+    // @notice nullifier = poseidon(nonce, secret)
+    mapping (bytes32 => bytes32) public partialNullifiers; // nullifier -> encryptedRemintedAmount 
+    //mapping(bytes32 => bool) public nullifiers;
     // smolverifier doesnt go down the full 248 depth of the tree but is able to run witn noir js (and is faster)
     address public smolVerifier;
     address public fullVerifier;
@@ -27,6 +29,7 @@ contract Token is ERC20, Ownable {
     // @workaround since BLOCKHASH opcode is useless
     // https://docs.scroll.io/en/technology/chain/differences/#opcodes
     mapping (uint256 => bytes32) public trustedBlockhash;
+
     //function setBlockHash(bytes32 blockHash, uint256 blockNum) public onlyOwner {
     function setBlockHash(bytes32 blockHash, uint256 blockNum) public {
         trustedBlockhash[blockNum] = blockHash;
@@ -70,14 +73,14 @@ contract Token is ERC20, Ownable {
     }
 
     //---------------public---------------------
-    function reMint(address to, uint256 amount, uint256 blockNum, bytes32 nullifier, bytes calldata snarkProof) public {
-        _reMint( to,  amount,  blockNum, nullifier, snarkProof,  smolVerifier);
+    function reMint(address to, uint256 amount, uint256 blockNum, bytes32 partialNullifier, bytes32 encryptedRemintedAmount, bytes calldata snarkProof) public {
+        _reMint( to,  amount,  blockNum, partialNullifier, encryptedRemintedAmount, snarkProof,  smolVerifier);
     }
 
     // just incase the contracts leaf will sit deeper than 53
     // or less likely the storage tree becomes deeper than 53
-    function reMintFullVerifier(address to, uint256 amount, uint256 blockNum, bytes32 nullifier, bytes calldata snarkProof) public {
-        _reMint( to,  amount,  blockNum, nullifier, snarkProof,  fullVerifier);
+    function reMintFullVerifier(address to, uint256 amount, uint256 blockNum, bytes32 partialNullifier, bytes32 encryptedRemintedAmount, bytes calldata snarkProof) public {
+        _reMint( to,  amount,  blockNum, partialNullifier, encryptedRemintedAmount, snarkProof,  fullVerifier);
     }
 
     // verifier wants the [u8;32] (bytes32 array) as bytes32[32] array.
@@ -88,10 +91,10 @@ contract Token is ERC20, Ownable {
     //TODO make private
     // TODO see much gas this cost and if publicInputs can be calldata
     // does bit shifting instead of indexing save gas?
-    function _formatPublicInputs(address to, uint256 amount, bytes32 blkhash, bytes32 nullifier) public pure returns (bytes32[] memory) {
+    function _formatPublicInputs(address to, uint256 amount, bytes32 blkhash, bytes32 partialNullifier, bytes32 encryptedRemintedAmount) public pure returns (bytes32[] memory) {
         bytes32 amountBytes = bytes32(uint256(amount));
         bytes32 toBytes = bytes32(uint256(uint160(bytes20(to))));
-        bytes32[] memory publicInputs = new bytes32[](66);
+        bytes32[] memory publicInputs = new bytes32[](67);
         publicInputs[0] = toBytes;
         for (uint i=1; i < 33; i++) {
             publicInputs[i] = bytes32(uint256(uint8(amountBytes[i-1])));
@@ -99,19 +102,21 @@ contract Token is ERC20, Ownable {
         for (uint i=33; i < 65; i++) {
             publicInputs[i] = bytes32(uint256(uint8(blkhash[i-33])));
         }
-        publicInputs[65] = nullifier;
+        publicInputs[65] = partialNullifier;
+        publicInputs[66] = encryptedRemintedAmount;
         return publicInputs;
     }
 
-    function _reMint(address to, uint256 amount, uint256 blockNum, bytes32 nullifier, bytes calldata snarkProof, address _verifier) private {
-        require(nullifiers[nullifier] == false, "burn address already used");
-        nullifiers[nullifier] = true;
+    function _reMint(address to, uint256 amount, uint256 blockNum, bytes32 partialNullifier, bytes32 encryptedRemintedAmount, bytes calldata snarkProof, address _verifier) private {
+        //require(nullifiers[nullifier] == false, "burn address already used");
+        require(partialNullifiers[partialNullifier] == bytes32(0x0), "nullifier already exist");
+        partialNullifiers[partialNullifier] = encryptedRemintedAmount;
 
         // @workaround
         //blockhash() is fucking use less :,(
         //bytes32 blkhash = blockhash(blockNum);
         bytes32 blkhash = trustedBlockhash[blockNum];
-        bytes32[] memory publicInputs = _formatPublicInputs(to, amount, blkhash, nullifier);
+        bytes32[] memory publicInputs = _formatPublicInputs(to, amount, blkhash, partialNullifier, encryptedRemintedAmount);
         if (!IVerifier(_verifier).verify(snarkProof, publicInputs)) {
             revert VerificationFailed();
         }
